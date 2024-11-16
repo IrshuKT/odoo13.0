0a7fb8ab39f41d5b8dcaf8306a45a2d4e4ce73d8
IrshuKT <irshad2934@gmail.com>
2024-11-16 21:29:32 +0530

    odoo 13

---
+++ dev/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name' : 'Clinic',
+    'author': "KT'S Hub",
+    'depends': ['base','web'],
+    'data':[],
+    'installable': True,  # Important for the module to be installable
+    'application': True,  # Makes it appear in the Apps menu
+    'auto_install': False,  # Set to False unless you want auto-installation
+}
